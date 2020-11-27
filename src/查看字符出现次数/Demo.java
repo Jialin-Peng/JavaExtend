90ebe777f6d86fcaf926fb544e95a69060978cc8
Jialin-Peng <931869033@qq.com>
2020-11-27 21:24:16 +0800

    导入字符出现次数

---
+++ src/查看字符出现次数/Demo.java
@@ -0,0 +1,42 @@
+package 查看字符出现次数;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//输入字符串
+		System.out.print("请输入一个字符串：");
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		//创建map集合
+		Map<Character,Integer> map=new HashMap<Character,Integer>();
+		/*
+			将字符存入map集合，并判断
+			如果集合中不存在该字符，则在该集合中存入该字符
+			如果集合中已经存在该字符，则在该集合中将该字符的值+1
+		*/
+		for(int i=0;i<s.length();i++) {
+			char key=s.charAt(i);
+			Integer value=map.get(key);
+			if(map.get(key)==null) {
+				map.put(key, 1);
+			}else {
+				value++;
+				map.put(key, value);
+			}
+		}
+		//遍历map集合
+		System.out.print("字符(出现次数)：");
+		for(Character key:map.keySet()) {
+		String result=key+"("+map.get(key)+")";
+		System.out.print(result);
+		}
+		sc.close();
+	}
+
+}
